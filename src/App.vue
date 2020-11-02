353b8205589f5d62c7cbb45893a86afb5a478a7b
SytronXar <aydemir_emre65@hotmail.com>
2020-11-02 13:57:21 +0300

    Güncelleme 2.11.20 13.56

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ export default {
 }
 </script>
 <style>
+
+@import './Style.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/TheWrapper.vue
+++ src/components/TheWrapper.vue
@@ -1,145 +1,144 @@
-
 <template>
-
-    <div >
-        <TheTopBar/>
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="navbar-header__content">
-                <div class="navbar-header__area">
-                    <router-link id="navbar_brand" to="/">
-                        <img src="https://www.vatanbilgisayar.com/assets/dist/images/vatanlogo.svg"/>
-                    </router-link>
-                </div>
-                <div class="navbar-header__area">
-                    <div class="search-form">
-                        <input autocomplete="off" id="navbar-search-input" class="search__input" type="search"  placeholder="Aramak istediğiniz ürünü yazın">
-                        <button class="search-button">
-                            <span class="icon-search">
-                                <i class="fa fa-search"></i>
-                            </span>
-                        </button>
-                    </div>
-                </div>
-                <div class="navbar-header__area">
-                    <button type="button" id="btnMyAccount" class="btn btn-primary btn-login dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-user"></i>
-                        <span id="type">GİRİŞ YAP</span>
+  <div>
+    <TheTopBar />
+    <div class="wrapper-topbar-menu menu-toptools">
+      <div class="global-container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="navbar-header__content no-padding">
+              <div class="navbar-header__area">
+                <a class="navbar-brand" href="/">
+                  <img
+                    src="https://www.vatanbilgisayar.com/assets/dist/images/vatanlogo.svg"
+                    alt="Vatan Bilgisayar"
+                  />
+                </a>
+              </div>
+              <div class="navbar-header__area">
+                <div class="search search-light hide">
+                  <div class="search__form">
+                    <input
+                      autocomplete="off"
+                      data-searchUrl="/Partial/Search"
+                      id="navbar-search-input"
+                      class="search__input"
+                      type="text"
+                      name="search"
+                      placeholder="Aramak istediğiniz ürünü yazın"
+                    />
+                    <a href="javascript:void(0);" class="remove__button">
+                      <span class="icon-times"></span>
+                    </a>
+                    <button class="search__button">
+                      <span class="icon-search">
+                        <i class="fa fa-search"></i>
+                      </span>
                     </button>
+                  </div>
                 </div>
+              </div>
+              <div class="navbar-header__area">
+                <div class="wrap-button">
+                  <div class="btn-group my-account">
+                    <button
+                      type="button"
+                      id="btnMyAccount"
+                      class="btn btn-primary btn-login dropdown-toggle"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      @focus="OpenDropdown"
+                      @blur="CloseDropdown"
+                    >
+                      <span class="icon-user"><i class="fas fa-user"></i></span>
+                      <span id="type">GİRİŞ</span>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-home hide account">
+                      <li><a href="/uyeBilgi/uyeBilgi">Üyeliğim</a></li>
+                      <li><a href="/uyeBilgi/siparistakip">Siparişlerim</a></li>
+                      <li>
+                        <a href="/uyeBilgi/favorilistem">Favori Ürünlerim</a>
+                      </li>
+                      <li><a href="/uyeBilgi/uyeAdres">Adres Bilgilerim</a></li>
+                      <li><a href="/uyeBilgi/mesaj">Mesajlarım</a></li>
+                      <li>
+                        <a
+                          href="/MemberTransaction/Logout?returnUrl=%2F&amp;logtab=signup"
+                          >ÇIKIŞ <span class="icon-door-open"></span
+                        ></a>
+                      </li>
+                    </ul>
+                    <ul class="dropdown-menu dropdown-menu-home hide login">
+                      <li>
+                        <a href="/login?returnUrl=%2F&amp;logtab=signin"
+                          >Giriş Yap</a
+                        >
+                      </li>
+                      <li>
+                        <a href="/login?returnUrl=%2F&amp;logtab=signup"
+                          >Üye Ol</a
+                        >
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div class="btn-group referrer-search-area">
+                    <i class="icon-search"></i>
+                  </div>
+
+                  <div class="btn-group basketGroup">
+                    <button
+                      type="button"
+                      id="btnMyBasket"
+                      class="btn btn-primary btn-basket dropdown-toggle"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <span class="icon-shopping-card"
+                        ><i class="fas fa-shopping-cart"></i
+                      ></span>
+                      <span class="btn-basket--text">SEPETİM</span>
+                      <span class="btn-basket--count">0</span>
+                    </button>
+                    <ul
+                      class="dropdown-menu dropdown-menu-basket openBasket"
+                    ></ul>
+                  </div>
+                </div>
+              </div>
+              <div id="navbar-search-results"></div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <style scoped>
-    #navbar-header__area{
-    display: table-cell;
-    vertical-align: middle;
-    width: 33.333333%;
-    }
-    #navbar_brand{
-        background-color: #fff;
-        margin: 12px 0;
-        margin-top: 12px;
-        margin-right: 0px;
-        margin-bottom: 12px;
-        margin-left: 0px;
-        padding: 0;
-        padding-top: 0px;
-        padding-right: 0px;
-        padding-bottom: 0px;
-        padding-left: 0px;
-    }
-    #navbar_brand img{
-        height: 50px;
-    }
-    #navbar-header__content {
-        position: relative;
-        display: table;
-        height: 100%;
-        width: 100%;
-    }
-    #search-form{
-        position: relative;
-        max-width: 623px;
-        width: 100%;
-        margin: 0 auto;
-    }
-    #search__input{
-        width: 100%;
-        border: none;
-        background-color: #efefef;
-        border-radius: 30px;
-        outline: 0;
-        padding: 10px 50px 11px 15px;
-        font-size: 14px;
-        color: #1c1c1c;
-        opacity: .5;
-        border: 1px solid transparent;
-    }
-    #search_button{
-        position: absolute;
-        top: 0;
-        right: 20px;
-        border: none;
-        background: 0 0;
-        outline: 0;
-        line-height: 41px;
-        margin: 0;
-        padding: 0;
-    }
-    
-    #search_button img{
-        height: 10;
-        width: 10;
-    }
-    #icon-search:before {
-    content: "\e940";
-    }  
-    #btn{
-        display: inline-block;
-        margin-bottom: 0;
-        font-weight: 400;
-        text-align: center;
-        white-space: nowrap;
-        vertical-align: middle;
-        -ms-touch-action: manipulation;
-        touch-action: manipulation;
-        cursor: pointer;
-        background-image: none;
-        border: 1px solid transparent;
-        padding: 6px 12px;
-        font-size: 14px;
-        line-height: 1.42857143;
-        border-radius: 4px;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-    } 
-    #btn-primary{
-        background-color: #337ab7;
-        border-color: #2e6da4;
-        color: #fff;
-        font-weight: 700;
-    }
-    .btn-login {
-        position: relative;
-    }
-    .btn.btn-primary.dropdown-toggle {
-        padding: 10px 22px 10px 27px;
-    }
-    .btn-group, .btn-group-vertical {
-        position: relative;
-        display: inline-block;
-        vertical-align: middle;
-    }
-    
+#navbar_brand img {
+  height: 50px;
+}
+.button drop_down_arrow {
+  float: right;
+}
 </style>
 <script>
-import TheTopBar from "@/components/TheTopBar"
+import TheTopBar from "@/components/TheTopBar";
 export default {
-  components:{
+  components: {
     TheTopBar
+  },
+
+  methods: {
+    OpenDropdown(event) { 
+        var Target=event.target;
+        Target.setAttribute("aria-expanded", true);
+        Target.parentElement.classList.add("open");  
+    },
+    CloseDropdown(event) { 
+        var Target=event.target;
+        Target.setAttribute("aria-expanded", false);      
+        Target.parentElement.classList.remove("open");  
+    }
   }
-}
+};
 </script>
