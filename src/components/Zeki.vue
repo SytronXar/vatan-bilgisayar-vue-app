c4433bf453c562111ff33896b9ae01146d820943
SytronXar <aydemir_emre65@hotmail.com>
2020-11-04 17:00:23 +0300

    Banner gizleme

--- src/components/Zeki.vue
+++ src/components/Zeki.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="global-container">
+  <div class="global-container" v-show="bannerActive">
     <a href="https://zeki.vatanbilgisayar.com/">
       <img
         src="https://cdn.vatanbilgisayar.com/Upload/BANNER//zeki/zeki2.jpg"
@@ -7,9 +7,33 @@
         class="img-responsive small-banner__image lazy-init"
       />
     </a>
-    <div id="smallBannerHide" class="small-banner__button">
+    <div id="smallBannerHide" class="small-banner__button" @click="bannerActive=false">
       <span class="small-banner__text">Gizle</span>
-      <span class="icon-times"></span>
+      <span ><i class="fas fa-times fa-lg"></i></span>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+    bannerActive:true,
+    }
+  },
+  components: {
+    
+  },
+  computed:{
+
+  },
+  props:{
+
+  },
+  methods: {
+    RemoveBanner(event) { 
+        var Target=event.target;     
+        Target.classList.elementParent.remove("");  
+    }
+  }
+};
+</script>
