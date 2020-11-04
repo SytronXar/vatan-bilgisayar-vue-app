1a63ea4f3a051eb277ad1e8787f0949811621563
SytronXar <aydemir_emre65@hotmail.com>
2020-11-04 15:03:56 +0300

    main güncelleme

---
+++ src/components/Zeki.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="global-container">
+    <a href="https://zeki.vatanbilgisayar.com/">
+      <img
+        src="https://cdn.vatanbilgisayar.com/Upload/BANNER//zeki/zeki2.jpg"
+        alt="small banner"
+        class="img-responsive small-banner__image lazy-init"
+      />
+    </a>
+    <div id="smallBannerHide" class="small-banner__button">
+      <span class="small-banner__text">Gizle</span>
+      <span class="icon-times"></span>
+    </div>
+  </div>
+</template>
