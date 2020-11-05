d66bc89fad344da2d0a25412c4f75168aa8ce456
SytronXar <aydemir_emre65@hotmail.com>
2020-11-05 23:07:16 +0300

    Product data eklendi

---
+++ src/Products.js
@@ -0,0 +1,93 @@
+export default {
+    Products: [
+        {
+            id: 111981,
+            code: "UX363EA-EM045T",
+            name: "ASUS ZENBOOK 13 UX363 CORE İ7 1165G7 2.8GHZ-16GB RAM-1TB SSD-13.3'-INT-W10",
+            cost: 19635,
+            productLaunchTime: '2020-11-05', /* ürünün satışa sunulma tarihi */
+            rate: 0, /* yıldız */
+            ratedPersonCount: 0, /* puanlayan kişi sayısı */
+            freeShipping: true, /* ücretsiz kargo */
+            fastShipping: false, /* hızlı kargo */
+            maxInstallmentCount: 6, /* taksit sayısı */
+            /* bu ürünle tavsiye edilenler:*/
+            advicedProducts: [
+                88816,
+            ],
+            images: [
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/111981_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/111981-1_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/111981-2_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/111981-3_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/111981-4_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/111981-5_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/111981-6_large.jpg"
+            ]
+        },
+
+        /* tavsiye edilen üç ürün yazdım tüm notebooklara bu üç ürünü tavsiye edebiliriz belki  */
+        {
+            id: 88816,
+            code: "SDRW-08U9M-U-BLACK",
+            name: "ASUS SDRW-08U9M-U ZENDRIVE HARİCİ SLİM DVD-YAZICI - SİYAH",
+            cost: 413,
+            productLaunchTime: '2020-08-10',
+            rate: 5,
+            ratedPersonCount: 17,
+            freeShipping: true,
+            fastShipping: false,
+            maxInstallmentCount: 6,
+            advicedProducts: [
+
+            ],
+            images: [
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/v2-88816_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/v2-88816-1_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/v2-88816-2_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/v2-88816-3_large.jpg"
+            ]
+        },
+        {
+            id: 84459,
+            code: "QQ2-00006",
+            name: "Online Microsoft 365 Bireysel (1 kullanıcı-1yıl) (Dijital İndirilebilir Lisans)",
+            cost: 550,
+            productLaunchTime: '2020-08-10',
+            rate: 5,
+            ratedPersonCount: 17,
+            freeShipping: true,
+            fastShipping: false,
+            maxInstallmentCount: 6,
+            advicedProducts: [
+
+            ],
+            images: [
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/microsoft/thumb/TeoriV2-84459_large.jpg",
+            ]
+        },
+        {
+            id: 104988,
+            code: "MB16AMT ",
+            name: "ASUS 15,6' MB16AMT ZenScreen taşınabilir Dokunmatik IPS FullHD USB monitör",
+            cost: 4355,
+            productLaunchTime: '2020-08-10',
+            rate: 0,
+            ratedPersonCount: 0,
+            freeShipping: true,
+            fastShipping: false,
+            maxInstallmentCount: 6,
+            advicedProducts: [
+
+            ],
+            images: [
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/TeoriV2-104988_large.jpg", ,
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/TeoriV2-104988-1_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/TeoriV2-104988-2_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/TeoriV2-104988-3_large.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/asus/thumb/TeoriV2-104988-4_large.jpg"
+            ]
+        },
+        /* Diğer notebooklar */
+    ]
+}
